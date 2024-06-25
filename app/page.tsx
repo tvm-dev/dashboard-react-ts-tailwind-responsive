e4a2b36ae966d7253ed532088a7bd88dc2cfac17
Thiago Viana <tvm.dev2015@gmail.com>
2024-06-25 08:46:25 -0300

    Dashboard with prints done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <>
-      <h2>Dashboard</h2>
+      <p>Dummy page</p>
     </>
-  );
+  )
 }
---
+++ components/sidebar-logo.tsx
@@ -0,0 +1,17 @@
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+export const SideBarLogo = () => {
+  const { theme } = useTheme();
+  return (
+    <Image
+      width={35}
+      alt=""
+      className="w-12 mx-3.5 min-h-fit"
+      height={35}
+      src={
+        theme === "dark" || theme === "custom" ? "/logo3.webp" : "/logo3.webp"
+      }
+    />
+  );
+};
